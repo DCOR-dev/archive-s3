46eedf4e92b6a1ba1e4291cf69c6ba36630b376c
Paul Müller <dev@craban.de>
2024-05-13 15:25:30 +0200

    enh: add mail command for failed script

--- archive-s3.sh
+++ archive-s3.sh
@@ -3,6 +3,10 @@
 #
 #   * */6 * * * ~/repos/archive-s3/archive-s3.sh >> /dev/null 2>&1
 #
+# Or, if you can send emails, do this:
+#
+#   * */6 * * * ~/repos/archive-s3/archive-s3.sh 2> >(/usr/bin/mail -s "S3 archive errors" recipient@example.com)
+#
 set -e
 set -x
 
