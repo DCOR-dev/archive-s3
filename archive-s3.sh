c88f8145199715a7dabc4980fb875be1d4153b22
Paul Müller <dev@craban.de>
2024-06-14 23:56:17 +0200

    update cron script

--- archive-s3.sh
+++ archive-s3.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 # Create a cron job for this bash script with the following content:
 #
+#   ARCHIVE_MAIL=mymail@example.com
 #   0 */6 * * * ~/repos/archive-s3/archive-s3.sh >> /dev/null 2>&1
 #
-# Or, if you can send emails, do this:
-#
-#   0 */6 * * * ~/repos/archive-s3/archive-s3.sh > /dev/null 2> (/usr/bin/mail -s "S3 archive errors" recipient@example.com)
-#
-set -e
-set -x
-
+LOGFILE=$(mktemp)
 source ~/env_archive_s3/bin/activate
-python ~/repos/archive-s3/archive-s3.py
+
+# run the python script
+python ~/repos/archive-s3/archive-s3.py > "$LOGFILE" 2>&1
+
+# check for errors
+retVal=$?
+if [ $retVal -ne 0 ] && [ -n "$ARCHIVE_MAIL" ]; then
+    /usr/bin/mail -s "S3 archive errors" "$ARCHIVE_MAIL" < "$LOGFILE"
+fi
+
+# cleanup
+rm -f $LOGFILE
+
+exit $retVal
